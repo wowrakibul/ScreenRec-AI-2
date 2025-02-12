5213e2e5ea35d8025a3e3f268171aef678832488
Wow Rakibul <74898633+wowrakibul@users.noreply.github.com>
2025-02-12 16:22:46 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 const CONFIG = {
-    GITHUB_TOKEN: 'your_github_token_here',
-    GITHUB_REPO: 'wowrakibul/ScreenRec-AI-2',
-    GITHUB_BRANCH: 'main'
+    SUPABASE_URL: 'https://qcpqehkiyniybxqxrwwn.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjcHFlaGtpeW5peWJ4cXhyd3duIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkzNTI2ODAsImV4cCI6MjA1NDkyODY4MH0.9L5FIgOJRKSI7nWfcPIXJz_L2ELWbVMmZ0XZRzD_kWg',
+    BUCKET_NAME: 'RecScreenAl',
+    USERNAME: 'wowrakibul',
+    CURRENT_DATE: '2025-02-12'
 };
